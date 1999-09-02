bc2ae329e46d92158ce1155c9c81f5b2d85d0c7e
src <empty>
1999-09-02 07:13:05 +0000

    Initial revision

---
+++ file.c
@@ -0,0 +1,37 @@
+#include <gnome.h>
+#include "menu.h"
+
+#define APPNAME "derived"
+#define VERSION "0.0.1"
+#define WINNAME "Derived"
+
+void
+destroy (GtkWidget * pWidget, gpointer * pData)
+{
+	gtk_main_quit();
+}
+
+int
+main (int argc, char *argv[])
+{
+	GtkWidget   *app    = NULL;
+
+	gnome_init (APPNAME, VERSION, argc, argv);
+
+	app = gnome_app_new (APPNAME, WINNAME);
+
+	gtk_signal_connect (GTK_OBJECT (app), "destroy", GTK_SIGNAL_FUNC (destroy), NULL);
+
+	gtk_window_set_default_size	(GTK_WINDOW (app), 200, 500);
+	menu_create			(GNOME_APP  (app));
+
+	gtk_widget_show_all (app);
+	gtk_main();
+
+	return 0;
+}
+
+void main_diff (void)
+{
+	g_print ("main_diff\n");
+}
---
+++ file.h
@@ -0,0 +1,9 @@
+#ifndef _FILE_H_
+#define _FILE_H_
+
+#include <gnome.h>
+
+void main_diff (void);
+
+#endif // _FILE_H_
+
