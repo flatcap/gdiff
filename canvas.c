838d99eb10056b0de366bf67bc1b1639d2096d18
src <empty>
1999-09-22 04:16:12 +0000

    scaling works

--- canvas.c
+++ canvas.c
@@ -18,12 +18,13 @@ item_event (GnomeCanvasItem * item, GdkEvent * event, gpointer data)
 	GdkCursor      *fleur;
 	static int      dragging;
 	double          item_x, item_y;
+	double          orig_x, orig_y;
 
-	item_x = event->button.x;
-	item_y = event->button.y;
-	g_print ("event (%d, %d)\n", (int) item_x, (int) item_y);
+	orig_x = item_x = event->button.x;
+	orig_y = item_y = event->button.y;
 	gnome_canvas_item_w2i (item->parent, &item_x, &item_y);
 
+	//g_print ("event (%d, %d) world (%d, %d)\n", (int) item_x, (int) item_y, (int) orig_x, (int) orig_y);
 
 	switch (event->type)
 	{
@@ -75,18 +76,24 @@ item_event (GnomeCanvasItem * item, GdkEvent * event, gpointer data)
 
 void size_request (GtkWidget *widget, GtkRequisition *requisition)
 {
-	g_print ("request %dx%d\n", requisition->width, requisition->height);
+	//g_print ("request %dx%d\n", requisition->width, requisition->height);
 }
 
 void size_allocate (GtkWidget *widget, GtkAllocation *allocation)
 {
+	int    i = MIN (allocation->width, allocation->height);
+	double d = i;
+
 	g_print ("alloc   %dx%d at (%d, %d)\n", allocation->width, allocation->height, allocation->x, allocation->y);
+
+	gtk_layout_set_size              (GTK_LAYOUT   (widget), i, i);
+	gnome_canvas_set_scroll_region   (GNOME_CANVAS (widget), 0.0, 0.0, 1.0, 1.0);
+	gnome_canvas_set_pixels_per_unit (GNOME_CANVAS (widget), d);
 }
 
-int
-main (int argc, char *argv[])
+void
+table_canvas (GtkWidget *app)
 {
-	GtkWidget        *app    = NULL;
 	GtkWidget        *canvas = NULL;
 	GnomeCanvasGroup *group  = NULL;
 	GnomeCanvasItem  *item   = NULL;
@@ -94,10 +101,6 @@ main (int argc, char *argv[])
 	GtkWidget        *label1 = NULL;
 	GtkWidget        *label2 = NULL;
 	GtkWidget        *scroll = NULL;
-
-	gnome_init (APP, VER, argc, argv);
-
-	app = gnome_app_new (APP, TTL);
 
 	table	= gtk_table_new (1, 2, FALSE);
 
@@ -109,7 +112,7 @@ main (int argc, char *argv[])
 	gtk_widget_pop_colormap ();
 	gtk_widget_pop_visual ();
 
-	gnome_canvas_set_scroll_region (GNOME_CANVAS (canvas), 0, 0, 100, 300);
+	gnome_canvas_set_scroll_region (GNOME_CANVAS (canvas), 0.0, 0.0, 100.0, 300.0);
 	gtk_layout_set_size (GTK_LAYOUT (canvas), 100, 300);
 
 	group = gnome_canvas_root (GNOME_CANVAS (canvas));
@@ -142,8 +145,62 @@ main (int argc, char *argv[])
 
 	gtk_window_set_default_size (GTK_WINDOW (app), 300, 300);
 	gnome_app_set_contents (GNOME_APP (app), table);
-	gtk_widget_show_all (app);
+}
 
+void
+scaled_canvas (GtkWidget *app)
+{
+	GtkWidget        *canvas = NULL;
+	GnomeCanvasGroup *group  = NULL;
+	GnomeCanvasItem  *item   = NULL;
+
+	gtk_signal_connect (GTK_OBJECT (app), "destroy", (GtkSignalFunc) destroy, NULL);
+
+	gtk_widget_push_visual (gdk_rgb_get_visual ());
+	gtk_widget_push_colormap (gdk_rgb_get_cmap ());
+	canvas = gnome_canvas_new_aa ();
+	gtk_widget_pop_colormap ();
+	gtk_widget_pop_visual ();
+
+	gnome_canvas_set_scroll_region (GNOME_CANVAS (canvas), 0.0, 0.0, 1.0, 1.0);
+	gtk_layout_set_size (GTK_LAYOUT (canvas), 300, 300);
+
+	group = gnome_canvas_root (GNOME_CANVAS (canvas));
+
+	item = gnome_canvas_item_new (group,
+					gnome_canvas_rect_get_type (),
+					"x1", 0.01,
+					"y1", 0.01,
+					"x2", 0.99,
+					"y2", 0.99,
+					"fill_color_rgba", 0x3cb371FF,
+					//"outline_color", "pink",
+					//"width_units", 0.04,
+					NULL);
+	gtk_signal_connect (GTK_OBJECT (item), "event", (GtkSignalFunc) item_event, NULL);
+
+	gtk_signal_connect (GTK_OBJECT (canvas), "size_request",  (GtkSignalFunc) size_request,  NULL);
+	gtk_signal_connect (GTK_OBJECT (canvas), "size_allocate", (GtkSignalFunc) size_allocate, NULL);
+
+	gnome_canvas_set_pixels_per_unit (GNOME_CANVAS (canvas), 300.0);
+
+	gtk_window_set_default_size (GTK_WINDOW (app), 300, 300);
+	gnome_app_set_contents (GNOME_APP (app), canvas);
+}
+
+int
+main (int argc, char *argv[])
+{
+	GtkWidget        *app    = NULL;
+
+	gnome_init (APP, VER, argc, argv);
+
+	app = gnome_app_new (APP, TTL);
+
+	//table_canvas  (app);
+	scaled_canvas (app);
+
+	gtk_widget_show_all (app);
 	gtk_main();
 	return 0;
 }
