14dae6dc1ad44d8ed132a06a44e85313061075a3
src <empty>
1999-09-24 00:58:03 +0000

    Initial revision

---
+++ options.c
@@ -0,0 +1,3 @@
+
+this will be an object representing a diff's options
+there will also an 'option' help by the global object (the defaults)
---
+++ options.h
@@ -0,0 +1,7 @@
+#ifndef _OPTIONS_H_
+#define _OPTIONS_H_
+
+
+
+#endif // _OPTIONS_H_
+
