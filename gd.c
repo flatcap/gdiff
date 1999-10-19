f12debc182f56835ecc3ace20450b7a39b8d6649
src <empty>
1999-10-19 07:06:59 +0000

    copyright

--- gd.c
+++ gd.c
@@ -1,4 +1,23 @@
-/* $Revision: 1.11 $ */
+/*
+    gd.c -- description
+    Copyright (C) 1999  Richard Russon <richard.russon@ait.co.uk>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* $Revision: 1.12 $ */
 
 #include "config.h"
 #include <gnome.h>
