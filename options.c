813bf26c4f5696d627c7623ff5dd6a3be49f4ef5
src <empty>
1999-10-24 10:01:27 +0000

    .

--- options.c
+++ options.c
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-/* $Revision: 1.19 $ */
+/* $Revision: 1.20 $ */
 
 #include "config.h"
 #include "options.h"
@@ -415,6 +415,75 @@ help_signal (GtkWidget *button, gpointer data)
 	g_free (help.path);
 }
 
+//XXX move this an exclusions to their own files entirely
+static void
+add_style_page (GnomePropertyBox *props)
+{
+	static char *text[] = { "List Same", "List Left", "List Right", "List Diff", "List Error / Type", "File Left", "File Right", NULL };
+	GtkWidget *frame     = gtk_frame_new ("Style");
+	GtkWidget *padding   = gtk_vbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *label     = gtk_label_new ("Style");
+	GtkWidget *hbox      = gtk_hbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *clist     = gtk_clist_new (1);
+	GtkWidget *vbox      = gtk_vbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *entry     = gtk_entry_new ();
+	GtkWidget *fg_box    = gtk_hbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *bg_box    = gtk_hbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *fg_check  = gtk_check_button_new_with_label ("Foreground");
+	GtkWidget *bg_check  = gtk_check_button_new_with_label ("Background");
+	GtkWidget *fg_dummy  = gtk_label_new ("");
+	GtkWidget *bg_dummy  = gtk_label_new ("");
+	GtkWidget *fg_colour = gnome_color_picker_new();
+	GtkWidget *bg_colour = gnome_color_picker_new();
+	int i;
+
+	gtk_clist_set_selection_mode     (GTK_CLIST (clist), GTK_SELECTION_BROWSE);
+	gtk_clist_set_column_auto_resize (GTK_CLIST (clist), 0, TRUE);
+	for (i = 0; text[i]; i++)
+	{
+		gtk_clist_append (GTK_CLIST (clist), &text[i]);
+	}
+
+	gtk_entry_set_text (GTK_ENTRY (entry), _("Sample text"));
+	gtk_widget_set_sensitive (entry, FALSE);
+
+	gtk_box_pack_start (GTK_BOX (fg_box), fg_check,  FALSE, FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (fg_box), fg_dummy,  TRUE,  TRUE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (fg_box), fg_colour, FALSE, FALSE, GNOME_PAD_SMALL);
+
+	gtk_box_pack_start (GTK_BOX (bg_box), bg_check,  FALSE, FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (bg_box), bg_dummy,  TRUE,  TRUE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (bg_box), bg_colour, FALSE, FALSE, GNOME_PAD_SMALL);
+
+	gtk_box_pack_start (GTK_BOX (vbox), entry,  FALSE, FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (vbox), fg_box, FALSE, FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (vbox), bg_box, FALSE, FALSE, GNOME_PAD_SMALL);
+
+	gtk_box_pack_start (GTK_BOX (hbox), clist, FALSE, FALSE, GNOME_PAD_SMALL);
+	gtk_box_pack_start (GTK_BOX (hbox), vbox,  FALSE, FALSE, GNOME_PAD_SMALL);
+
+	gtk_box_pack_start (GTK_BOX (padding), hbox, TRUE, TRUE, GNOME_PAD_SMALL);
+
+	gtk_container_add  (GTK_CONTAINER (frame), padding);
+
+	gnome_property_box_append_page (props, frame, label);
+}
+
+static void
+add_exlusions_page (GnomePropertyBox *props)
+{
+	GtkWidget *frame   = gtk_frame_new ("Exclusions");
+	GtkWidget *padding = gtk_vbox_new  (FALSE, GNOME_PAD_SMALL);
+	GtkWidget *label   = gtk_label_new ("Exclusions");
+	GtkWidget *vbox    = gtk_vbox_new  (FALSE, GNOME_PAD_BIG);
+
+	gtk_box_pack_start (GTK_BOX (padding), vbox, TRUE, TRUE, GNOME_PAD_SMALL);
+
+	gtk_container_add  (GTK_CONTAINER (frame), padding);
+
+	gnome_property_box_append_page (props, frame, label);
+}
+
 GtkWidget *
 get_preferences (GtkWindow *parent, PrefsPage /*XXX unused*/page_sel)
 {
@@ -465,6 +534,9 @@ get_preferences (GtkWindow *parent, PrefsPage /*XXX unused*/page_sel)
 				break;
 		}
 	}
+
+	add_style_page     (props);
+	add_exlusions_page (props);
 
 	gtk_window_set_transient_for (GTK_WINDOW (props), parent);
 
